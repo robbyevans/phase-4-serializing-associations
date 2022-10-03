81f6a528a36b14ec2f19841f7c681e0dd6fdb97c
Evans Rutto <robbyevans001@gmail.com>
2022-10-03 06:40:20 +0300

    commit tree 87cd48835345dff0e8b83b2d6c599a8adccaec4c

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,20 +1,13 @@
 class DirectorsController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
     directors = Director.all
-    render json: directors
+    render json: directors, include: ['movies', 'movies.reviews']
   end
 
   def show
     director = Director.find(params[:id])
-    render json: director
-  end
-
-  private
-
-  def render_not_found_response
-    render json: { error: "Director not found" }, status: :not_found
+    render json: director, include: ['movies', 'movies.reviews']
   end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,3 +2,8 @@ class Movie < ApplicationRecord
   belongs_to :director
   has_many :reviews
 end
+
+# app/models/review.rb
+class Review < ApplicationRecord
+  belongs_to :movie
+end
